c91f2ef2649f2ba0d802b3906502cb589b45b29d
Himanshi745 <166390857+Himanshi745@users.noreply.github.com>
2025-08-10 22:44:06 +0530

    Update Signup.jsx

--- Event-Management-Site/src/components/Signup.jsx
+++ Event-Management-Site/src/components/Signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://localhost:5000/api/auth/signup", {
+    const res = await fetch("https://event-management-miu9.onrender.com/api/auth/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData)
