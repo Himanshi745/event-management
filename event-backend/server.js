336f00391e9c7d0e74a01d2ffe64899f6d32155f
Himanshi745 <166390857+Himanshi745@users.noreply.github.com>
2025-08-12 09:30:37 +0530

    Update server.js

--- event-backend/server.js
+++ event-backend/server.js
@@ -16,6 +16,9 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+app.use(express.static(path.join(__dirname, "public")));
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
